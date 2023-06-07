d289381667a6fd537c2c94d560cedbe547959719
Luis Tellez <luis@luistellez.com>
2023-06-07 12:06:04 -0700

    🚀 chore(product.js): export Product model from product.js module to improve code organization and reusability

--- src/models/product.js
+++ src/models/product.js
@@ -8,3 +8,7 @@ const productSchema = new Schema({
   isNew: { type: Boolean, required: true },
   description: { type: String, required: true },
 });
+
+const Product = mongoose.model("Product", productSchema);
+
+module.exports = Product;
