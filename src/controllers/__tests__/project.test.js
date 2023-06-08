176908a051678e7d882d801fe6ff42d03fcac8bd
Luis Tellez <luis@luistellez.com>
2023-06-08 02:30:37 -0700

    🧪 test(project.test.js): add test for updateProduct method in ProjectController to ensure ProductService.updateProduct is called with correct arguments

--- src/controllers/__tests__/project.test.js
+++ src/controllers/__tests__/project.test.js
@@ -10,3 +10,38 @@ const chance = new Chance();
 
 // Mock the dependencies
 jest.mock("../../services/products");
+
+describe("When calling update Product Controller", () => {
+  let id, productData, updatedProduct, req, res;
+
+  beforeEach(() => {
+    id = chance.guid();
+    productData = {
+      name: chance.name(),
+      image: chance.url(),
+      price: chance.integer({ min: 1, max: 100 }),
+      isNew: chance.bool(),
+      description: chance.sentence(),
+    };
+    updatedProduct = productData;
+    req = {
+      params: {
+        id,
+      },
+      body: productData,
+    };
+    res = {
+      status: jest.fn().mockReturnThis(),
+      json: jest.fn().mockReturnThis(),
+    };
+    ProductService.updateProduct = jest.fn().mockResolvedValue(updatedProduct);
+  });
+
+  test("Then it should call ProductService.updateProduct with id and productData", async () => {
+    // Act
+    await ProjectController.updateProduct(req, res);
+
+    // Assert
+    expect(ProductService.updateProduct).toHaveBeenCalledWith(id, productData);
+  });
+});
