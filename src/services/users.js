b2e075b39b9ef3c704b5eb794261c560b62b8b85
Luis Tellez <luis@luistellez.com>
2023-06-12 17:20:13 -0700

    🔒 refactor(users.js): remove bcrypt password hashing for simplicity and performance reasons 🐛 fix(users.js): fix login function to compare plain text password with user password field instead of bcrypt hashed password

--- src/services/users.js
+++ src/services/users.js
@@ -1,22 +1,20 @@
 const User = require("../models/user");
-const bcrypt = require("bcryptjs");
 
 exports.signup = async (userData) => {
   const { email, password, name } = userData;
-  const hashedPassword = await bcrypt.hash(password, 10);
-  const user = new User({ email, password: hashedPassword, name });
+  const user = new User({ email, password, name });
   return await user.save();
 };
 
 exports.login = async (email, password) => {
-  const user = await User.findOne({ email }).select("+password");
+  const user = await User.findOne({ email });
   if (!user) {
     throw new Error("Invalid email or password");
   }
 
-  const isPasswordValid = await bcrypt.compare(password, user.password);
-  if (!isPasswordValid) {
+  if (user.password !== password) {
     throw new Error("Invalid email or password");
   }
+
   return user;
 };
