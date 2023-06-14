7a4559106e051c810177f2f407cfd6e54cc171af
Luis Tellez <luis@luistellez.com>
2023-06-13 21:46:35 -0700

    🐛 fix(users.js): hash user password before saving to database to improve security ✨ feat(users.js): add input validation to signup function to improve data integrity and prevent invalid input errors. Add check for existing user with same email address to prevent duplicate users. Add password hashing to login function to improve security.

--- src/services/users.js
+++ src/services/users.js
@@ -1,20 +1,49 @@
 const User = require("../models/user");
+const bcrypt = require("bcrypt");
 
 exports.signup = async (userData) => {
+  // Validate that the user data is valid
   const { email, password, name } = userData;
-  const user = new User({ email, password, name });
-  return await user.save();
+  if (!email || !password || !name) {
+    throw new Error("Invalid input");
+  }
+
+  // Check to see if the user email address already exists in the database
+  const existingUser = await User.findOne({ email });
+  if (existingUser) {
+    throw new Error("User with that email address already exists");
+  }
+
+  // Hash the user password
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  // Create a new user
+  const user = new User({
+    email,
+    password: hashedPassword,
+    name,
+  });
+
+  // Save the user to the database
+  await user.save();
+
+  // Return the user
+  return user;
 };
 
 exports.login = async (email, password) => {
+  // Check to see if the user exists
   const user = await User.findOne({ email });
   if (!user) {
     throw new Error("Invalid email or password");
   }
 
-  if (user.password !== password) {
+  // Check if the password is correct
+  const isPasswordValid = await bcrypt.compare(password, user.password);
+  if (!isPasswordValid) {
     throw new Error("Invalid email or password");
   }
 
+  // Return the user
   return user;
 };
